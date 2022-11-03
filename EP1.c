6949cc2a847035492f0daca386ad676b03219da5
luca-moraes <lcansd.lm@gmail.com>
2022-11-03 02:07:38 -0300

    stdin e stdout

---
+++ EP1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    char word[10];
+    int kQuantWords = 0;
+
+    scanf("%s \n %d", word, &kQuantWords);
+
+    printf("Word: %s\nNum: %d", word, kQuantWords);
+
+    //printf("Hello, World!\n");
+    return 0;
+}
