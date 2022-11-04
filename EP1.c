a35582d77c86d7b49d8486d1d7375104337e49c2
luca-moraes <lcansd.lm@gmail.com>
2022-11-04 03:23:50 -0300

    requisite 1 automaton mount function development initiated

--- EP1.c
+++ EP1.c
@@ -1,22 +1,44 @@
-#include <stdio.h>
 #include <stdbool.h>
+#include <stdio.h>
+// #include <stdlib.h>
 
 typedef struct States State;
 
-struct States{
+struct States {
     char name[10];
-    State *transictionA;
-    struct States *transictionB;
+    // State *transictionA;
+    // struct States *transictionB;
+    State *transictions[2];
     bool finalState;
 } state;
 
-int main() {
-    char word[10];
-    int kQuantWords = 0;
+void mountAutomaton(State *automaton[10], char *word[10], int wordSize){
+    int i;
+    for(i = 0; i < wordSize; i++){
+        State newState;
 
+        *newState.name = *word[0];
+        *automaton[i] = newState;
+    }
+}
+
+int main() {
+    char word[10
+            // +1
+    ];
+    // char quantWords[4+1];
+    int kQuantWords = 0;
+    State automaton[10];
+
+    //TODO: jogar a entrada no buffer com tamanho antes da variavel;
+    // fgets(word, sizeof(word)-1, stdin);
+    // fgets(quantWords, sizeof(quantWords)-1, stdin);
+    // kQuantWords = atoi(quantWords);
+
+    // scanf(fgets(word, 10, stdin), word);
     scanf("%s \n %d", word, &kQuantWords);
 
-    char testWords[kQuantWords][200];
+    char testWords[kQuantWords][200+1];
 
     int i;
     for (i = 0; i < kQuantWords; ++i) {
@@ -30,6 +52,6 @@ int main() {
         printf("Test word %d: %s\n", j, testWords[j]);
     }
 
-    //printf("Hello, World!\n");
+    // printf("Hello, World!\n");
     return 0;
 }
