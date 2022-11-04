df18f10e4de97990b499011456d9f90fc8453a1d
luca-moraes <lcansd.lm@gmail.com>
2022-11-04 19:13:55 -0300

    unversionedted files clion

---
+++ cmake-build-debug/CMakeFiles/projetoAutomatos.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/projetoAutomatos.dir/EP1.c.o"
+  "CMakeFiles/projetoAutomatos.dir/EP1.c.o.d"
+  "projetoAutomatos"
+  "projetoAutomatos.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/projetoAutomatos.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
